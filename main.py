61bb3f024b07a1cab318cd6766c6cf87aa7c1aa7
nao-riku <76162238+nao-riku@users.noreply.github.com>
2023-02-18 05:02:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import chromedriver_binary
 from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 import datetime
-import json
-import copy
-import requests
 import os
-import sys
 
 
 
