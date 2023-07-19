a5079ae7290b7f170cabae1be720f9aa63656d77
nao-riku <76162238+nao-riku@users.noreply.github.com>
2023-07-20 02:44:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -468,7 +468,7 @@ def start():
         else:
             break
 
-    login_twitter("rank334_2", os.environ['PASS'], os.environ['TEL'], driver)
+    login_twitter("rank334", os.environ['PASS'], os.environ['TEL'], driver)
     get_334(driver)
          
 start()
